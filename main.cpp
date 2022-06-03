b9258c3b85dc16011ddb94daf29f72e7da9afe0a
hakan karadeniz <hakan.karadeniz@outlook.com>
2022-06-03 21:49:27 +0300

    03.06.2022

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "main.h"
+//////////////////////////////////////////////////////////////////////////////
+int main(){
+
+    vector<string> data;
+    entering_app();
+
+    int command;
+    cin >> command;
+
+    switch (command)
+    {   case 1: adding(); break;
+        case 2:
+        case 3:
+    
+    default:
+        break;
+    }
+
+    
+    
+//////////////////////////////////////////////////////////////////////////////
+    
+    
+
+    return 0;
+}
