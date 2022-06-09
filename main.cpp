05ddde2f6c4d29b7d9144d629687b9fbc5536b0c
hakan karadeniz <hakan.karadeniz@outlook.com>
2022-06-09 15:32:02 +0300

    09.06.2022

--- main.cpp
+++ main.cpp
@@ -2,118 +2,118 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <cstring>
 using namespace std;
 
+void menu()
+{
 
-void menu() {
-    cout << "  Please Enter Your Command " << endl;
     cout << "  1> Adding New Registration " << endl;
     cout << "  2> Delete Registration " << endl;
     cout << "  3> Sort The Registrations" << endl;
     cout << "  4> Print the all DATA  " << endl;
     cout << "  5> To EXIT the Program " << endl;
+    cout << "  Please Enter Your Command " << endl;
 }
 
-void print_data(){
+struct registration
+{
+    int id_num, phone_num;
+    string name, surname, mail;
+};
 
-    char n;
+vector<registration> app;
+
+int registration_save()
+{
+    ofstream data_write;
     ifstream file_read;
     file_read.open("main_file.txt");
-    file_read >> noskipws;
-    while (file_read >> n)
+    data_write.open("main_file.txt", ios::app);
+
+
+    int pre_data{0};
+    cout << "Please Enter Student's ID No: ";
+    cin >> pre_data;
+
+    while (!file_read.eof())
     {
-       cout << n;
+        registration adding;
+        file_read >> adding.id_num >> adding.name >> adding.surname >> adding.mail >> adding.phone_num;
+
+        app.push_back(adding);
     }
-    
-    if(n == '1')
-        cout << "       Press >1 to Back Main menu  /" << endl;    
-        cin >> n;
+    file_read.close();
+
+    int ok{1};
+    int i = 0;
+    while (i < app.size())
+    {
+        if (app[i].id_num==pre_data)
+        {
+
+            ok = 0;
+            break;
+        }
+        i++;
+    }
+
+    if (ok)
+    {
+        registration adding;
+
+        cout << "Please Enter Student's Name: ";
+        cin >> adding.name;
+        cout << endl;
+        cout << "Please Enter Student's Surname: ";
+        cin >> adding.surname;
+        cout << endl;
+        cout << "Please Enter Student's e-mail: ";
+        cin >> adding.mail;
+        cout << endl;
+        cout << "Please Enter Student's Phone Number: ";
+        cin >> adding.phone_num;
+        cout << endl;
+        adding.id_num=pre_data;
+        data_write << adding.id_num << " " << adding.name << " " << adding.surname << " " << adding.mail << " " << adding.phone_num << endl;
+
+        app.push_back(adding);
+    }
+        data_write.close();
+
+    return 0;
 }
 
+int main()
+{
 
+    int menuChoice{-1};
 
-void student_adding(vector<string> data){
-    
-    
-    ofstream file_write;
-    file_write.open("main_file.txt",ios::app);
-
-    string name;
-    string surname;
-    string id_num;
-    string phone_num;
-    string mail;
-
-    cout << "Please Enter Student's Name :" ;
-    cin >> name;
-    cout << endl;
-
-    cout << "Please Enter Student's Surname :";
-    cin >> surname;
-    cout << endl;
-
-    cout << "Please Enter Student's ID Number:";
-    cin >> id_num;
-    cout << endl;
-
-    cout << "Please Enter Student's Phone Number:";
-    cin >> phone_num;
-    cout << endl;
-
-    cout << "Please Enter Student's E-mail Address :";
-    cin >> mail;
-    cout << endl;
-    
-
-    data.insert(data.begin(),name);
-    data.insert(data.end(),surname);
-    data.insert(data.end(),id_num);
-    data.insert(data.end(),phone_num);
-    data.insert(data.end(),mail);
-
-    file_write << name + " ";
-    file_write << surname + " ";
-    file_write << id_num + " ";
-    file_write << phone_num + " ";
-    file_write << mail + "\n";
-
-}
-
-
-
-
-int main(){
-
-    vector<string> data;
-    int menuChoice = 0;
-    
-
-    while(menuChoice != 5){ 
+    while (menuChoice)
+    {
         menu();
         cin >> menuChoice;
 
         switch (menuChoice)
         {
         case 1:
-            student_adding(data);
+            registration_save();
             break;
         case 2:
             break;
         case 3:
+
             break;
         case 4:
-            print_data();
+
             break;
         case 5:
             cout << "Exited The Program" << endl;
+            menuChoice = 0;
             break;
         default:
             cout << "Please Enter True Command" << endl;
             break;
         }
-
     }
-
-    
-
 }
